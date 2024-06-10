cce23405560e45a4d6a6b3cf2d0b1ebcac5d617d
Djouko <destructeurkratos@gmail.com>
2024-06-10 08:47:25 +0100

    Initial commit

---
+++ src/main/java/sn/esmt/tasks/taskmanager/dto/converters/RegisterUser.java
@@ -0,0 +1,77 @@
+package sn.esmt.tasks.taskmanager.dto.converters;
+
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+public class RegisterUser {
+    @NotBlank
+    private String username;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
+    @NotBlank
+    private String confirmPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
